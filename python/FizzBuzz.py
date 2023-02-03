2a80f5b064722f91b742c1bd68bdbfc9ae9290d0
Gonzalo Alarcón Usui <66294948+Gonxolo@users.noreply.github.com>
2023-02-03 19:48:38 +0000

    style: indentations and variables' case

--- python/FizzBuzz.py
+++ python/FizzBuzz.py
@@ -1,16 +1,16 @@
-iterations = 100
+"""FizzBuzz implementation on Python3"""
 
-for i in range(1,iterations+1):
-  say = ''
-  
-  if i%3 == 0:
-    say += 'Fizz'
-  if i%5 == 0:
-    say += 'Buzz'
-  
-  if say == '':
-    print(i)
-  else:
-    print(say)
+ITERATIONS = 100
 
-  
+for i in range(1, ITERATIONS + 1):
+    say = ''
+
+    if i%3 == 0:
+        say += 'Fizz'
+    if i%5 == 0:
+        say += 'Buzz'
+
+    if say == '':
+        print(i)
+    else:
+        print(say)
