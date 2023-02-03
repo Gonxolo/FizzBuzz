ec25db9a52a55751fe3e36fdc81c761c032b6938
Gonzalo Alarcón Usui <66294948+Gonxolo@users.noreply.github.com>
2023-02-03 19:47:46 +0000

    feat: javascript implementation

---
+++ javascript/FizzBuzz.js
@@ -0,0 +1,20 @@
+const iterations = 100;
+
+for (let i=1; i<=iterations; i++) {
+
+    let say = '';
+
+    if (i%3 == 0) {
+        say += 'Fizz';
+    }
+    if (i%3 == 0) {
+        say += 'Buzz';
+    }
+
+    if (say === '') {
+        console.log(i);
+    } else {
+        console.log(say);
+    }
+
+}
